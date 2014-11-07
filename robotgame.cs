9ee6c4bba20c215d7f4749479e7f7758e717485d
orizilber <orizilber@users.noreply.github.com>
2014-11-07 14:33:40 +0200

    Delete robotgame.cs

--- robotgame.cs
+++
@@ -1,5 +0,0 @@
-class RobotGameXTREMEeditionEPICrpgMMOftwOMGeEEEEEEEEEEEEEEEEEEEEEPICCCC
-{
-    private int numOfBlocks;
-    private string[] robots = {"Optimus Prime", "Bumblebee", "barricade", "jazz"};
-}
